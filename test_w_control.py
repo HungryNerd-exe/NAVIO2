1d919140dedf7bf5282b8a16ec27e4fef86abe05
Brandon <bcwhitecb@gmail.com>
2019-11-22 05:39:13 +0000

    Rotate p q r

--- test_w_control.py
+++ test_w_control.py
@@ -207,6 +207,8 @@ def estimator_loop(y, xh, servo):
         q = y[gyro_q] - gyro_bias[1]
         r = y[gyro_r] - gyro_bias[2]
 
+	[p, q, r] = np.dot(R_imu, np.array([[p], [q], [r]]))
+
         if new_gps:
             [v_n_old, v_e_old, v_d_old] = [v_n, v_e, v_d]
             [p_n, p_e, p_d, v_n, v_e, v_d] = [y[gps_posn_n], y[gps_posn_e], y[gps_posn_d], y[gps_vel_n], y[gps_vel_e],
@@ -248,7 +250,7 @@ def estimator_loop(y, xh, servo):
         # ALL CODE ABOVE THIS LINE
         # ==================================================
         # DONE: Log X Hat, Servos, RCs, Y to CSV
-        f_logfile.write(', , '.join(map(str, xh)) + ', , '.join(map(str, servo)) + ', , '.join(map(str, y)) + '\n')
+        f_logfile.write(', '.join(map(str, xh)) + ', ' + ', '.join(map(str, servo)) + ', ' + ', '.join(map(str, y)) + '\n')
         # >>> TDB
 
 
